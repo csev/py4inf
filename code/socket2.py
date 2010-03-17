56b7562723a12e35c762bf1c0627595b8d8cf540
csev@umich.edu <csev@umich.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2010-03-17 20:45:11 +0000

    More on 13/14
    git-svn-id: https://source.sakaiproject.org/contrib/csev/trunk/pyinf/tex@66615 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ code/socket2.py
@@ -0,0 +1,13 @@
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(("localhost", 8080))
+mysock.send("GET http://localhost:8080/ HTTP/1.0\n\n")
+
+while True:
+    data = mysock.recv(512)
+    if ( len(data) < 1 ) :
+        break
+    print data;
+
+mysock.close()
---
+++ code/twitter3.py
@@ -0,0 +1,24 @@
+import urllib
+import xml.etree.ElementTree as ET
+
+TWITTER_URL = 'http://api.twitter.com/l/statuses/friends/ACCT.xml'
+
+while True:
+    print ''
+    acct = raw_input('Enter Twitter Account:')
+    if ( len(acct) < 1 ) : break
+    url = TWITTER_URL.replace('ACCT', acct)
+    print 'Retrieving', url
+    document = urllib.urlopen (url).read()
+    print 'Retrieved', len(document), 'characters.' 
+    tree = ET.fromstring(document)
+    count = 0
+    for user in tree.findall('user'):
+        count = count + 1
+        if count > 4 : break
+        print user.find('screen_name').text
+        print '  ',user.find('location').text
+        status =  user.find('status')
+        if status : 
+            txt = status.find('text').text
+            print '  ',txt[:50]
