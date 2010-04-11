b66a37bdb4f2ff8bead429e8d837b5fa7fe2e900
csev@umich.edu <csev@umich.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2010-04-11 16:56:32 +0000

    Work on the chapter on automating common tasks.
    git-svn-id: https://source.sakaiproject.org/contrib/csev/trunk/pyinf/tex@67089 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ code/txtcheck.py
@@ -0,0 +1,16 @@
+import os
+from os.path import join
+for (dirname, dirs, files) in os.walk('.'):
+   for filename in files:
+       if filename.endswith('.txt') :
+           thefile = os.path.join(dirname,filename)
+           size = os.path.getsize(thefile)
+           if size == 2578 or size == 2565:
+               continue
+           fhand = open(thefile,"r")
+           lines = list()
+           for line in fhand:
+               lines.append(line)
+           if len(lines) > 1:
+                print len(lines), thefile
+                print lines[:4]
