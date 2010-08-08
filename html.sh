793433aed5922943802a86d17fd7db045500f0a7
csev@umich.edu <csev@umich.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2010-08-08 20:24:32 +0000

    Playing with the HTML options using hevea
    git-svn-id: https://source.sakaiproject.org/contrib/csev/trunk/pyinf/tex@69669 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- html.sh
+++ html.sh
@@ -2,10 +2,10 @@
 
 rm -rf html
 mkdir html
-hevea -O -e latexonly htmlonly book
+hevea -O -e latexonly /opt/local/lib/hevea/html/png.hva htmlonly book
 # the following line is a kludge to prevent imagen from seeing
 # the definitions in latexonly
 grep -v latexonly book.image.tex > a; mv a book.image.tex
 imagen -png book
 hacha book.html
-mv index.html book.css book*.html book*.png *motif.gif html
+mv index.html book.css book*.html book*.png book*.gif *motif.gif html
