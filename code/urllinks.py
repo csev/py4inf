c779cbbf998daa4f71953cb3ea7eb3346b891dca
csev@umich.edu <csev@umich.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2010-11-03 05:01:11 +0000

    Fix missing ()
    git-svn-id: https://source.sakaiproject.org/contrib/csev/trunk/pyinf/tex@71145 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- code/urllinks.py
+++ code/urllinks.py
@@ -2,7 +2,7 @@ import urllib
 from BeautifulSoup import *
 
 url = raw_input('Enter - ')
-html = urllib.urlopen(url).read
+html = urllib.urlopen(url).read()
 soup = BeautifulSoup(html)
 
 # Retrieve all of the anchor tags
