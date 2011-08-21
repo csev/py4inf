a602f960acecc6b102ca6d7e5418d52cae9e2075
csev@umich.edu <csev@umich.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2011-08-21 17:10:55 +0000

    First cut at the Chapter 1 rewrite.
    git-svn-id: https://source.sakaiproject.org/contrib/csev/trunk/pyinf/tex@76172 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- html.sh
+++ html.sh
@@ -2,7 +2,7 @@
 
 rm -rf html
 mkdir html
-hevea -O -e latexonly /opt/local/lib/hevea/html/png.hva htmlonly book
+hevea -O -e latexonly png.hva htmlonly book
 # the following line is a kludge to prevent imagen from seeing
 # the definitions in latexonly
 grep -v latexonly book.image.tex > a; mv a book.image.tex
