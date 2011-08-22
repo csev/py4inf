a0f852b1fb88c1719b8a31bab8c2f57a7d7acc20
csev@umich.edu <csev@umich.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2011-08-22 00:49:27 +0000

    Refactoring the book into several files to help organize the rewrite tasks.
    Fix bits in the rewritten Chapter 1.
    
    
    
    git-svn-id: https://source.sakaiproject.org/contrib/csev/trunk/pyinf/tex@76175 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- book.sh
+++ book.sh
@@ -1,16 +1,9 @@
 #! /bin/sh
 
-latex book
-makeindex book
-latex book
-dvipdf book.dvi book.pdf
+latex cfbook
+makeindex cfbook
+latex cfbook
+dvipdf cfbook.dvi book.pdf
 open book.pdf
 echo Removed temporary files
-rm -f book.aux book.ind book.ilg book.log book.dvi book.idx book.toc book.haux book.hind book.image.tex
-exit
-
-latex book
-makeindex book
-latex book
-dvips -t letter -Ppdf -o thinkpython.ps book
-open thinkpython.ps
+rm -f cfbook.aux cfbook.ind cfbook.ilg cfbook.log cfbook.dvi cfbook.idx cfbook.toc cfbook.haux cfbook.hind cfbook.image.tex
