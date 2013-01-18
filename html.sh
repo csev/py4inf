2f6f9e1f3b240c99c2f575cc4828b7e8c0247308
csev@umich.edu <csev@umich.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2013-01-18 22:49:24 +0000

    Updtate
    git-svn-id: https://source.sakaiproject.org/contrib/csev/trunk/pyinf/tex@82455 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- html.sh
+++ html.sh
@@ -2,10 +2,10 @@
 
 rm -rf html
 mkdir html
-hevea -O -e latexonly png.hva htmlonly book
+hevea -O -e latexonly png.hva htmlonly cfbook
 # the following line is a kludge to prevent imagen from seeing
 # the definitions in latexonly
-grep -v latexonly book.image.tex > a; mv a book.image.tex
-imagen -png book
-hacha book.html
-mv index.html book.css book*.html book*.png book*.gif *motif.gif html
+grep -v latexonly cfbook.image.tex > a; mv a cfbook.image.tex
+imagen -png cfbook
+hacha cfbook.html
+mv index.html cfbook.css cfbook*.html cfbook*.png cfbook*.gif *motif.gif html
