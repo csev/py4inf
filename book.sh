65f5f181f6f871569c334bdc9ccf6e597df97c99
csev@umich.edu <csev@umich.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2013-09-08 01:16:05 +0000

    More tweaks in preparation for CreateSpace.
    git-svn-id: https://source.sakaiproject.org/contrib/csev/trunk/pyinf/tex@84601 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- book.sh
+++ book.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
 
+rm *.tmp *.aux
 latex cfbook
 makeindex cfbook
 latex cfbook
 dvipdf cfbook.dvi book.pdf
 open book.pdf
 echo Removed temporary files
-rm -f cfbook.aux cfbook.ind cfbook.ilg cfbook.log cfbook.dvi cfbook.idx cfbook.toc cfbook.haux cfbook.hind cfbook.image.tex
+rm -f cfbook.aux cfbook.ind cfbook.ilg cfbook.log cfbook.dvi cfbook.idx cfbook.toc cfbook.haux cfbook.hind cfbook.image.tex cfbook.tmp
