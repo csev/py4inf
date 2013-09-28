bcba6f9ef1e69b64b8f5147286e5420dd762e49d
csev@umich.edu <csev@umich.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2013-09-28 12:33:22 +0000

    Revise Chapter 13 away from the Twitter APIs and toward Google geocoding APIs.
    git-svn-id: https://source.sakaiproject.org/contrib/csev/trunk/pyinf/tex@84743 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- book.sh
+++ book.sh
@@ -7,4 +7,4 @@ latex cfbook
 dvipdf cfbook.dvi book.pdf
 open book.pdf
 echo Removed temporary files
-rm -f cfbook.aux cfbook.ind cfbook.ilg cfbook.log cfbook.dvi cfbook.idx cfbook.toc cfbook.haux cfbook.hind cfbook.image.tex cfbook.tmp
+rm -f cfbook.aux cfbook.ind cfbook.ilg cfbook.log cfbook.dvi cfbook.idx cfbook.toc cfbook.haux cfbook.hind cfbook.image.tex cfbook.tmp cfbook.idv cfbook.4tc cfbook.lg cfbook.xref
