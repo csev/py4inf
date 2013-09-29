1c437ef8c2a1d63c13a61173523655de787577cc
Charles Severance <csev@umich.edu>
2013-09-29 00:07:01 -0400

    Add the geodata application sample.

---
+++ code/geodata/where.js
@@ -0,0 +1,14 @@
+myData = [
+[42.3396998,-71.08975, 'Northeastern University, 360 Huntington Avenue, Boston, MA 02115, USA'],
+[40.6963857,-89.6160811, 'Bradley University, 1501 West Bradley Avenue, Peoria, IL 61625, USA'],
+[32.7775,35.0216667, 'Technion, Viazman 87, Kesalsaba, 32000, Israel'],
+[18.4636552,73.8680647, 'Vishwakarma Institute of Technology, Upper Indira Nagar, Bibwewadi, Pune, Maharashtra 411037, India'],
+[46.8220275,-92.0836388, 'UMD Stores, University of Minnesota Duluth, Duluth, MN 55812, USA'],
+[42.4066804,-71.1195528, 'Tufts University Medford, 419 Boston Avenue, Medford, MA 02155, USA'],
+[-37.9152113,145.134682, 'Monash University Clayton Campus, Wellington Road, Clayton VIC 3800, Australia'],
+[53.2833333,69.3833333, 'Kokshetau, Kazakhstan'],
+[44.9375992,34.134893, 'ТАВРИЧЕСКИЙ НАЦИОНАЛЬНЫЙ УНИВЕРСИТЕТ ИМ. В.И. ВЕРНАДСКОГО, просп. Вернадського, 4, Simferopol, Crimea, Ukraine, 95000'],
+[41.4148853,2.1334233, 'UOC - Universitat Oberta de Catalunya, Avinguda del Tibidabo, 39, 08035 Barcelona, Spain'],
+[52.27669,104.277965, 'Irkutsk State University, bulvar Gagarina, 20, Irkutsk, Irkutsk Oblast, Russia, 664003'],
+[31.1993538,121.4296032, 'Shanghai Jiaotong University （West Gate）, 655 Panyu Road, Xuhui, Shanghai, China, 200030']
+];
