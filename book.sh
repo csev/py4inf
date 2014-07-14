9dbb8c9db3a6f2a4bf43e7b188aa2f3fed40f28d
Charles Severance <csev@umich.edu>
2014-07-14 08:15:58 -0400

    Tweaks fo multiple operating systems.

--- book.sh
+++ book.sh
@@ -5,6 +5,12 @@ latex cfbook
 makeindex cfbook
 latex cfbook
 dvipdf cfbook.dvi book.pdf
-open book.pdf
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  open book.pdf
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+  xdg-open book.pdf
+else
+  echo "Output on book.pdf"
+fi
 echo Removed temporary files
 rm -f cfbook.aux cfbook.ind cfbook.ilg cfbook.log cfbook.dvi cfbook.idx cfbook.toc cfbook.haux cfbook.hind cfbook.image.tex cfbook.tmp cfbook.idv cfbook.4tc cfbook.lg cfbook.xref
