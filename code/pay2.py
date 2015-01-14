1cb125ce7e47366b5fbc2d9c872a52558ea750c6
Charles Severance <csev@umich.edu>
2015-01-14 16:09:38 -0500

    Fix thanks to AG

--- code/pay2.py
+++ code/pay2.py
@@ -3,7 +3,7 @@ hours = float(inp)
 inp = raw_input('Enter Rate: ')
 rate = float(inp)
 if hours > 40:
-    pay = hours * rate + (hours - 40) * rate * 1.5
+    pay = hours * rate + (hours - 40) * rate * 0.5
 else:
     pay = hours * rate
 print 'Pay:', pay
